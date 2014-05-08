bf7bc6e4da3761d753e45165d3c9f3e6db1e4f9e
Andy Wilkinson <awilkinson@gopivotal.com>
2014-05-08 10:39:16 +0100

    Add a test that verifies the Platform pom's dependency management
    The test creates a pom that has a dependency on every artifact for which
    the platform provide's dependency management and builds it. The build
    will fail if any of the dependencies cannot be resolved.

---
+++ pom-tests/src/main/groovyScripts/verifyDependencies.groovy
@@ -0,0 +1,68 @@
+def targetDir = new File(project.build.outputDirectory).absoluteFile
+def effectivePom = new File(targetDir, "effective-pom.xml")
+def platformProjectDir = new File(project.basedir, "../platform").absoluteFile
+
+println "mvn help:effective-pom -Doutput=$effectivePom".execute(null, platformProjectDir).text
+
+def pomHeader = """
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<project xmlns=\"http://maven.apache.org/POM/4.0.0\"
+		xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+		xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">
+
+	<modelVersion>4.0.0</modelVersion>
+
+	<groupId>test</groupId>
+	<artifactId>test</artifactId>
+	<version>1.0.0.BUILD-SNAPSHOT</version>
+
+	<dependencyManagement>
+		<dependencies>
+			<dependency>
+				<groupId>io.spring.platform</groupId>
+				<artifactId>platform</artifactId>
+				<scope>import</scope>
+				<type>pom</type>
+				<version>1.0.0.BUILD-SNAPSHOT</version>
+			</dependency>
+		</dependencies>
+	</dependencyManagement>
+
+	<dependencies>"""
+
+def pomFooter = """    </dependencies>
+	<repositories>
+		<repository>
+			<id>spring-snapshots</id>
+			<name>Spring Snapshots</name>
+			<url>http://repo.spring.io/libs-snapshot</url>
+			<snapshots>
+				<enabled>true</enabled>
+			</snapshots>
+		</repository>
+	</repositories>
+</project>"""
+
+new File(targetDir, "pom.xml").withWriter { writer ->
+	writer.println pomHeader
+
+	def xml = new XmlSlurper().parseText(effectivePom.text)
+	xml.dependencyManagement.dependencies.dependency.each { dependency -> 
+		writer.println "		<dependency>"
+		writer.println "			<groupId>${dependency.groupId}</groupId>"
+		writer.println "			<artifactId>${dependency.artifactId}</artifactId>"
+		if (dependency.type.size()) {
+			writer.println "			<type>${dependency.type}</type>"
+		}
+		writer.println "		</dependency>"
+	}
+
+	writer.println pomFooter
+}
+
+def build = "mvn clean package".execute(null, targetDir)
+build.in.eachLine { println it }
+
+if (build.exitValue()) {
+	throw new IllegalStateException("Maven build of test project failed")
+}
