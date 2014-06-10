b877f0141828034551bea83b12bd5e314b2ea8db
Andy Wilkinson <awilkinson@gopivotal.com>
2014-06-10 16:30:03 +0100

    Update findDuplicates to get spring-boot-dependencies pom from a repo
    Previously, findDuplicates took two arguments: the path to 
    spring-boot-dependencies’ pom and the path to the platform’s pom. This
    commit updates it to obtain the former from
    repo.spring.io/libs-snapshot, having determined the required version by
    looking in the platform definition file, and to automatically use
    platform-bom/pom.xml for the latter

--- scripts/findDuplicates.groovy
+++ scripts/findDuplicates.groovy
@@ -1,14 +1,36 @@
+@Grab('org.yaml:snakeyaml:1.13')
+import org.yaml.snakeyaml.Yaml
+
+def getRootDir() {
+	@groovy.transform.SourceURI
+	def source
+	new File(source).parentFile.parentFile
+}
+
 def getId(dependency) {
 	"${dependency.groupId}:${dependency.artifactId}:${dependency.type}"
 }
 
-def dependencies = []
+def getBootVersion() {
+	def yaml = new Yaml().load(new File(rootDir, 'platform-definition.yaml').text)
+	yaml['platform_definition']['projects'].find{it['name'] == 'Spring Boot'}['version']
+}
 
-new XmlSlurper().parseText(new File(args[0]).text)
+def getDependenciesPom() {
+	def uri = "https://repo.spring.io/libs-snapshot/org/springframework/boot/spring-boot-dependencies/$bootVersion"
+	def version = bootVersion
+	if (version.endsWith('-SNAPSHOT')) {
+		def snapshot = new XmlSlurper().parse("$uri/maven-metadata.xml").versioning.snapshot
+		version = version.replace('-SNAPSHOT', "-$snapshot.timestamp-$snapshot.buildNumber")
+	}
+	new XmlSlurper().parse("$uri/spring-boot-dependencies-${version}.pom")
+}
+
+def dependencies = dependenciesPom
 		.dependencyManagement.dependencies.dependency
-		.each { dependencies << getId(it) }
+		.collect { getId(it)}
 
-new XmlSlurper().parseText(new File(args[1]).text)
+new XmlSlurper().parseText(new File(rootDir, 'platform-bom/pom.xml').text)
 		.dependencyManagement.dependencies.dependency
 		.collect { getId(it) }
 		.findAll { dependencies.contains(it) }
