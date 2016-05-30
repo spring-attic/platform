0b0df69f5c78967287cf02a2b34395be11e1fd54
Andy Wilkinson <awilkinson@pivotal.io>
2016-05-30 07:14:11 +0100

    Align with latest Spring Boot 1.4 snapshots

--- platform-tests/src/test/groovy/io/spring/platform/BomTests.groovy
+++ platform-tests/src/test/groovy/io/spring/platform/BomTests.groovy
@@ -38,7 +38,7 @@ public class BomTests {
 
 	@Test
 	void versionPropertiesAreNotOverridden() {
-		def expectedOverrides = [ 'spring-integration.version' ]
+		def expectedOverrides = [ ]
 		def versions = extractVersionsFromPom(bootDependenciesPom)
 
 		def overriddenVersions = [:]
