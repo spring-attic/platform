0ad4bef55d64103a7106bc74dd1eb7678f8a0964
Andy Wilkinson <awilkinson@pivotal.io>
2017-02-02 10:07:17 +0000

    Remove duplicate configuration of spring-kafka.version property
    See gh-510

--- platform-tests/src/test/groovy/io/spring/platform/BomTests.groovy
+++ platform-tests/src/test/groovy/io/spring/platform/BomTests.groovy
@@ -38,7 +38,7 @@ public class BomTests {
 
 	@Test
 	void versionPropertiesAreNotOverridden() {
-		def expectedOverrides = [ 'spring-kafka.version' ]
+		def expectedOverrides = [ ]
 		def versions = extractVersionsFromPom(bootDependenciesPom)
 
 		def overriddenVersions = [:]
